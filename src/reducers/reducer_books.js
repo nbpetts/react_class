4f5402d7a1543418cd332d63327163c8f5af0bf6
Nathan Petts <nathanpetts@Nathans-MacBook-Pro.local>
2017-11-14 14:45:53 -0800

    adds some reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The good parts'},
+    {title: 'Hitchhikers guide to the galaxy'},
+    {title: 'Shop Girl'},
+    {title: 'Cryptonomicon'}
+  ]
+}
