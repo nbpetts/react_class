d6fd6ef611ecabba1f7dfb2392e586bf33f162b6
Nathan Petts <nathanpetts@Nathans-MacBook-Pro.local>
2017-11-15 11:43:15 -0800

    updates to end of section

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Javascript: The good parts'},
-    {title: 'Hitchhikers guide to the galaxy'},
-    {title: 'Shop Girl'},
-    {title: 'Cryptonomicon'}
+    {title: 'Javascript: The good parts', pages: 100},
+    {title: 'Hitchhikers guide to the galaxy', pages: 200},
+    {title: 'Shop Girl', pages: 175},
+    {title: 'Cryptonomicon', pages: 12}
   ]
 }
