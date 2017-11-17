27ffee88bdc09073f843f6c64027be09272d11f6
Nathan Petts <nathanpetts@Nathans-MacBook-Pro.local>
2017-11-17 11:45:21 -0800

    adds google maps

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,18 @@
-export function selectBook(book) {
+import axios from 'axios';
+
+const API_KEY = '54af99c0298c2ab3e85748f6dc5d872f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
   return {
-    type: 'BOOK_SELECTED',
-    payload: book
+    type: FETCH_WEATHER,
+    payload: request
   };
+
 }
--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    {title: 'Javascript: The good parts', pages: 100},
-    {title: 'Hitchhikers guide to the galaxy', pages: 200},
-    {title: 'Shop Girl', pages: 175},
-    {title: 'Cryptonomicon', pages: 12}
-  ]
-}
